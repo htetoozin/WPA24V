72cbeb245eee7689d800bfa304977e847681de13
phpcrazy <phpcrazy@gmail.com>
2016-11-05 12:55:03 +0630

    Register Test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,47 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/backend/register", function() {
+	$credentials = [
+    	'email'    => 'phpcrazy@gmail.com',
+    	'password' => '123456',
+	];
+
+	$user = Sentinel::register($credentials);
+	$activation = Activation::create($user);
+	var_dump("http://wpa24v.dev/activate/". $user->id . "/" . $activation['code']);
+});
+
+Route::get("activate/{id}/{activate_code}", function($id, $activate_code){
+	$user = Sentinel::findById($id);
+
+	if (Activation::complete($user, $activate_code))
+	{
+    	return "Activation Successfully!";
+	}
+	else
+	{
+    	return "Not Good!";
+	}
+});
+
+Route::get('password-test', function(){
+	var_dump(md5("123456"));
+	var_dump(md5("123456"));
+	var_dump(sha1("123456"));
+	var_dump(sha1("123456"));
+	var_dump(bcrypt("123456"));
+	var_dump(bcrypt("123456"));
+});
+
+
+
+
+
+
+
+
+
+
+
