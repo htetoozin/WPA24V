7f38734d5344641692627a0991aba9e217359061
sithumaungmaung <sithu.mm88@gmail.com>
2016-11-19 11:58:17 +0630

    Add Photo Url

--- resources/views/backend/nav.blade.php
+++ resources/views/backend/nav.blade.php
@@ -166,7 +166,7 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="http://localhost:8000/dist/img/user2-soe.jpg" class="user-image" alt="User Image">
+              <img src="{{ asset('dist/img/user2-soe.jpg') }}" class="user-image" alt="User Image">
               <span class="hidden-xs">Sayar Soe</span>
             </a>
             <ul class="dropdown-menu">
